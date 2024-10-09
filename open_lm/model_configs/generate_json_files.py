e778d31c7b95ced7d20491324f2ded9fe1592c6f
waterpine <songbian@zju.edu.cn>
2024-10-09 00:32:06 -0500

    update model config

--- open_lm/model_configs/generate_json_files.py
+++ open_lm/model_configs/generate_json_files.py
@@ -1,47 +1,38 @@
 import json
 
 
-data_dict_14M = {
-    '14m_v1': {
-        "hidden_dim": 128,
+data_dict_61M = {
+    '61m_v1': {
+        "hidden_dim": 448,
+        "n_layers": 6,
+        "n_heads": 7,
+        "seq_len": 2048,
+        "vocab_size": 50432,
+        "post_embed_norm": False,
+        "weight_tying": False
+    },
+    '61m_v2': {
+        "hidden_dim": 504,
         "n_layers": 3,
-        "n_heads": 4,
+        "n_heads": 7,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '14m_v2': {
-        "hidden_dim": 128,
-        "n_layers": 4,
-        "n_heads": 4,
+    '61m_v3': {
+        "hidden_dim": 392,
+        "n_layers": 10,
+        "n_heads": 7,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '14m_v3': {
-        "hidden_dim": 120,
-        "n_layers": 7,
-        "n_heads": 4,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '14m_v4': {
-        "hidden_dim": 116,
-        "n_layers": 9,
-        "n_heads": 4,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '14m_v5': {
-        "hidden_dim": 132,
-        "n_layers": 2,
-        "n_heads": 4,
+    '61m_v4': {
+        "hidden_dim": 336,
+        "n_layers": 18,
+        "n_heads": 7,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
@@ -50,8 +41,8 @@ data_dict_14M = {
 }
 
 
-data_dict_79M = {
-    '79m_v1': {
+data_dict_80M = {
+    '80m_v1': {
         "hidden_dim": 512,
         "n_layers": 8,
         "n_heads": 8,
@@ -60,43 +51,25 @@ data_dict_79M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '79m_v2': {
-        "hidden_dim": 528,
-        "n_layers": 7,
+    '80m_v2': {
+        "hidden_dim": 448,
+        "n_layers": 13,
         "n_heads": 8,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '79m_v3': {
-        "hidden_dim": 488,
-        "n_layers": 9,
+    '80m_v3': {
+        "hidden_dim": 384,
+        "n_layers": 22,
         "n_heads": 8,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '79m_v4': {
-        "hidden_dim": 472,
-        "n_layers": 11,
-        "n_heads": 8,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '79m_v5': {
-        "hidden_dim": 544,
-        "n_layers": 6,
-        "n_heads": 8,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '79m_v6': {
+    '80m_v4': {
         "hidden_dim": 576,
         "n_layers": 5,
         "n_heads": 8,
@@ -105,9 +78,9 @@ data_dict_79M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '79m_v7': {
-        "hidden_dim": 592,
-        "n_layers": 4,
+    '80m_v5': {
+        "hidden_dim": 640,
+        "n_layers": 3,
         "n_heads": 8,
         "seq_len": 2048,
         "vocab_size": 50432,
@@ -128,25 +101,7 @@ data_dict_116M = {
         "weight_tying": False
     },
     '116m_v2': {
-        "hidden_dim": 660,
-        "n_layers": 9,
-        "n_heads": 10,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '116m_v3': {
-        "hidden_dim": 700,
-        "n_layers": 7,
-        "n_heads": 10,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '116m_v4': {
-        "hidden_dim": 740,
+        "hidden_dim": 720,
         "n_layers": 6,
         "n_heads": 10,
         "seq_len": 2048,
@@ -154,16 +109,16 @@ data_dict_116M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '116m_v5': {
-        "hidden_dim": 590,
-        "n_layers": 12,
+    '116m_v3': {
+        "hidden_dim": 800,
+        "n_layers": 4,
         "n_heads": 10,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '116m_v6': {
+    '116m_v4': {
         "hidden_dim": 560,
         "n_layers": 15,
         "n_heads": 10,
@@ -172,11 +127,20 @@ data_dict_116M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
+    '116m_v5': {
+        "hidden_dim": 480,
+        "n_layers": 24,
+        "n_heads": 10,
+        "seq_len": 2048,
+        "vocab_size": 50432,
+        "post_embed_norm": False,
+        "weight_tying": False
+    },
 }
 
 
-data_dict_163M = {
-    '163m_v1': {
+data_dict_164M = {
+    '164m_v1': {
         "hidden_dim": 768,
         "n_layers": 12,
         "n_heads": 12,
@@ -185,25 +149,25 @@ data_dict_163M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '163m_v2': {
-        "hidden_dim": 660,
-        "n_layers": 18,
+    '164m_v2': {
+        "hidden_dim": 672,
+        "n_layers": 17,
         "n_heads": 12,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '163m_v3': {
-        "hidden_dim": 840,
-        "n_layers": 9,
+    '164m_v3': {
+        "hidden_dim": 576,
+        "n_layers": 26,
         "n_heads": 12,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '163m_v4': {
+    '164m_v4': {
         "hidden_dim": 864,
         "n_layers": 8,
         "n_heads": 12,
@@ -212,17 +176,8 @@ data_dict_163M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '163m_v5': {
-        "hidden_dim": 900,
-        "n_layers": 7,
-        "n_heads": 12,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '163m_v6': {
-        "hidden_dim": 948,
+    '164m_v5': {
+        "hidden_dim": 960,
         "n_layers": 6,
         "n_heads": 12,
         "seq_len": 2048,
@@ -233,8 +188,8 @@ data_dict_163M = {
 }
 
 
-data_dict_232M = {
-    '232m_v1': {
+data_dict_237M = {
+    '237m_v1': {
         "hidden_dim": 896,
         "n_layers": 14,
         "n_heads": 14,
@@ -243,8 +198,8 @@ data_dict_232M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '232m_v2': {
-        "hidden_dim": 1022,
+    '237m_v2': {
+        "hidden_dim": 1008,
         "n_layers": 10,
         "n_heads": 14,
         "seq_len": 2048,
@@ -252,17 +207,8 @@ data_dict_232M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '232m_v3': {
-        "hidden_dim": 1218,
-        "n_layers": 6,
-        "n_heads": 14,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '232m_v4': {
-        "hidden_dim": 1092,
+    '237m_v3': {
+        "hidden_dim": 1120,
         "n_layers": 8,
         "n_heads": 14,
         "seq_len": 2048,
@@ -270,8 +216,17 @@ data_dict_232M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '232m_v5': {
-        "hidden_dim": 770,
+    '237m_v4': {
+        "hidden_dim": 1232,
+        "n_layers": 6,
+        "n_heads": 14,
+        "seq_len": 2048,
+        "vocab_size": 50432,
+        "post_embed_norm": False,
+        "weight_tying": False
+    },
+    '237m_v5': {
+        "hidden_dim": 784,
         "n_layers": 20,
         "n_heads": 14,
         "seq_len": 2048,
@@ -279,9 +234,9 @@ data_dict_232M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '232m_v6': {
-        "hidden_dim": 812,
-        "n_layers": 18,
+    '237m_v6': {
+        "hidden_dim": 672,
+        "n_layers": 31,
         "n_heads": 14,
         "seq_len": 2048,
         "vocab_size": 50432,
@@ -291,8 +246,8 @@ data_dict_232M = {
 }
 
 
-data_dict_309M = {
-    '309m_v1': {
+data_dict_313M = {
+    '313m_v1': {
         "hidden_dim": 1024,
         "n_layers": 16,
         "n_heads": 16,
@@ -301,7 +256,7 @@ data_dict_309M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '309m_v2': {
+    '313m_v2': {
         "hidden_dim": 1152,
         "n_layers": 12,
         "n_heads": 16,
@@ -310,36 +265,36 @@ data_dict_309M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '309m_v3': {
-        "hidden_dim": 1344,
-        "n_layers": 8,
+    '313m_v3': {
+        "hidden_dim": 1280,
+        "n_layers": 9,
         "n_heads": 16,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '309m_v4': {
-        "hidden_dim": 1232,
-        "n_layers": 10,
+    '313m_v4': {
+        "hidden_dim": 1408,
+        "n_layers": 7,
         "n_heads": 16,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '309m_v5': {
-        "hidden_dim": 960,
-        "n_layers": 19,
-        "n_heads": 16,
-        "seq_len": 2048,
-        "vocab_size": 50432,
-        "post_embed_norm": False,
-        "weight_tying": False
-    },
-    '309m_v6': {
-        "hidden_dim": 880,
+    '313m_v5': {
+        "hidden_dim": 896,
         "n_layers": 22,
+        "n_heads": 16,
+        "seq_len": 2048,
+        "vocab_size": 50432,
+        "post_embed_norm": False,
+        "weight_tying": False
+    },
+    '313m_v6': {
+        "hidden_dim": 768,
+        "n_layers": 33,
         "n_heads": 16,
         "seq_len": 2048,
         "vocab_size": 50432,
@@ -349,8 +304,8 @@ data_dict_309M = {
 }
 
 
-data_dict_455M = {
-    '455m_v1': {
+data_dict_469M = {
+    '469m_v1': {
         "hidden_dim": 1280,
         "n_layers": 16,
         "n_heads": 10,
@@ -359,16 +314,16 @@ data_dict_455M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '455m_v2': {
-        "hidden_dim": 1400,
-        "n_layers": 13,
+    '469m_v2': {
+        "hidden_dim": 1360,
+        "n_layers": 14,
         "n_heads": 10,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '455m_v3': {
+    '469m_v3': {
         "hidden_dim": 1440,
         "n_layers": 12,
         "n_heads": 10,
@@ -377,27 +332,27 @@ data_dict_455M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '455m_v4': {
-        "hidden_dim": 1540,
-        "n_layers": 10,
+    '469m_v4': {
+        "hidden_dim": 1520,
+        "n_layers": 11,
         "n_heads": 10,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '455m_v5': {
-        "hidden_dim": 1220,
-        "n_layers": 18,
+    '469m_v5': {
+        "hidden_dim": 1200,
+        "n_layers": 19,
         "n_heads": 10,
         "seq_len": 2048,
         "vocab_size": 50432,
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '455m_v6': {
-        "hidden_dim": 1150,
-        "n_layers": 21,
+    '469m_v6': {
+        "hidden_dim": 1120,
+        "n_layers": 23,
         "n_heads": 10,
         "seq_len": 2048,
         "vocab_size": 50432,
@@ -407,8 +362,8 @@ data_dict_455M = {
 }
 
 
-data_dict_695M = {
-    '695m_v1': {
+data_dict_712M = {
+    '712m_v1': {
         "hidden_dim": 1536,
         "n_layers": 19,
         "n_heads": 12,
@@ -417,8 +372,8 @@ data_dict_695M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '695m_v2': {
-        "hidden_dim": 1608,
+    '712m_v2': {
+        "hidden_dim": 1632,
         "n_layers": 17,
         "n_heads": 12,
         "seq_len": 2048,
@@ -426,8 +381,8 @@ data_dict_695M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '695m_v3': {
-        "hidden_dim": 1692,
+    '712m_v3': {
+        "hidden_dim": 1728,
         "n_layers": 15,
         "n_heads": 12,
         "seq_len": 2048,
@@ -435,8 +390,8 @@ data_dict_695M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '695m_v4': {
-        "hidden_dim": 1800,
+    '712m_v4': {
+        "hidden_dim": 1824,
         "n_layers": 13,
         "n_heads": 12,
         "seq_len": 2048,
@@ -444,7 +399,7 @@ data_dict_695M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '695m_v5': {
+    '712m_v5': {
         "hidden_dim": 1440,
         "n_layers": 22,
         "n_heads": 12,
@@ -453,8 +408,8 @@ data_dict_695M = {
         "post_embed_norm": False,
         "weight_tying": False
     },
-    '695m_v6': {
-        "hidden_dim": 1332,
+    '712m_v6': {
+        "hidden_dim": 1344,
         "n_layers": 26,
         "n_heads": 12,
         "seq_len": 2048,
@@ -465,18 +420,22 @@ data_dict_695M = {
 }
 
 
-for key in data_dict_14M.keys():
+for key in data_dict_61M.keys():
     filename = f"scale_open_lm_{key}.json"
     with open(filename, 'w') as json_file:
-        json.dump(data_dict_14M[key], json_file, indent=4)
-    assert data_dict_14M[key]["hidden_dim"] % data_dict_14M[key]["n_heads"] == 0
+        json.dump(data_dict_61M[key], json_file, indent=4)
+    assert data_dict_61M[key]["hidden_dim"] % data_dict_61M[key]["n_heads"] == 0
+    assert data_dict_61M[key]["n_heads"] == 7
+    assert data_dict_61M[key]["hidden_dim"] / data_dict_61M[key]["n_heads"] % 8 == 0
 
 
-for key in data_dict_79M.keys():
+for key in data_dict_80M.keys():
     filename = f"scale_open_lm_{key}.json"
     with open(filename, 'w') as json_file:
-        json.dump(data_dict_79M[key], json_file, indent=4)
-    assert data_dict_79M[key]["hidden_dim"] % data_dict_79M[key]["n_heads"] == 0
+        json.dump(data_dict_80M[key], json_file, indent=4)
+    assert data_dict_80M[key]["hidden_dim"] % data_dict_80M[key]["n_heads"] == 0
+    assert data_dict_80M[key]["n_heads"] == 8
+    assert data_dict_80M[key]["hidden_dim"] / data_dict_80M[key]["n_heads"] % 8 == 0
 
 
 for key in data_dict_116M.keys():
@@ -484,32 +443,51 @@ for key in data_dict_116M.keys():
     with open(filename, 'w') as json_file:
         json.dump(data_dict_116M[key], json_file, indent=4)
     assert data_dict_116M[key]["hidden_dim"] % data_dict_116M[key]["n_heads"] == 0
+    assert data_dict_116M[key]["n_heads"] == 10
+    assert data_dict_116M[key]["hidden_dim"] / data_dict_116M[key]["n_heads"] % 8 == 0
 
 
-for key in data_dict_232M.keys():
+for key in data_dict_164M.keys():
     filename = f"scale_open_lm_{key}.json"
     with open(filename, 'w') as json_file:
-        json.dump(data_dict_232M[key], json_file, indent=4)
-    assert data_dict_232M[key]["hidden_dim"] % data_dict_232M[key]["n_heads"] == 0
+        json.dump(data_dict_164M[key], json_file, indent=4)
+    assert data_dict_164M[key]["hidden_dim"] % data_dict_164M[key]["n_heads"] == 0
+    assert data_dict_164M[key]["n_heads"] == 12
+    assert data_dict_164M[key]["hidden_dim"] / data_dict_164M[key]["n_heads"] % 8 == 0
 
 
-for key in data_dict_309M.keys():
+for key in data_dict_237M.keys():
     filename = f"scale_open_lm_{key}.json"
     with open(filename, 'w') as json_file:
-        json.dump(data_dict_309M[key], json_file, indent=4)
-    assert data_dict_309M[key]["hidden_dim"] % data_dict_309M[key]["n_heads"] == 0
+        json.dump(data_dict_237M[key], json_file, indent=4)
+    assert data_dict_237M[key]["hidden_dim"] % data_dict_237M[key]["n_heads"] == 0
+    assert data_dict_237M[key]["n_heads"] == 14
+    assert data_dict_237M[key]["hidden_dim"] / data_dict_237M[key]["n_heads"] % 8 == 0
 
 
-for key in data_dict_455M.keys():
+for key in data_dict_313M.keys():
     filename = f"scale_open_lm_{key}.json"
     with open(filename, 'w') as json_file:
-        json.dump(data_dict_455M[key], json_file, indent=4)
-    assert data_dict_455M[key]["hidden_dim"] % data_dict_455M[key]["n_heads"] == 0
+        json.dump(data_dict_313M[key], json_file, indent=4)
+    assert data_dict_313M[key]["hidden_dim"] % data_dict_313M[key]["n_heads"] == 0
+    assert data_dict_313M[key]["n_heads"] == 16
+    assert data_dict_313M[key]["hidden_dim"] / data_dict_313M[key]["n_heads"] % 8 == 0
 
 
-for key in data_dict_695M.keys():
+for key in data_dict_469M.keys():
     filename = f"scale_open_lm_{key}.json"
     with open(filename, 'w') as json_file:
-        json.dump(data_dict_695M[key], json_file, indent=4)
-    assert data_dict_695M[key]["hidden_dim"] % data_dict_695M[key]["n_heads"] == 0
+        json.dump(data_dict_469M[key], json_file, indent=4)
+    assert data_dict_469M[key]["hidden_dim"] % data_dict_469M[key]["n_heads"] == 0
+    assert data_dict_469M[key]["n_heads"] == 10
+    assert data_dict_469M[key]["hidden_dim"] / data_dict_469M[key]["n_heads"] % 8 == 0
+
+
+for key in data_dict_712M.keys():
+    filename = f"scale_open_lm_{key}.json"
+    with open(filename, 'w') as json_file:
+        json.dump(data_dict_712M[key], json_file, indent=4)
+    assert data_dict_712M[key]["hidden_dim"] % data_dict_712M[key]["n_heads"] == 0
+    assert data_dict_712M[key]["n_heads"] == 12
+    assert data_dict_712M[key]["hidden_dim"] / data_dict_712M[key]["n_heads"] % 8 == 0
 
